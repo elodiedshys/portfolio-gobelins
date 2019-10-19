042309f18f8f12bb178cd8412aaff223304bbac9
Elodie Deshayes <elodied69@gmail.com>
2019-10-19 23:20:08 +0200

    first commit

---
+++ drag.js
@@ -0,0 +1,11 @@
+$(function() {
+  $( "#draggable" ).draggable({stack: 'div'});
+  $('#draggable').addClass('shadow');
+
+});
+
+  $(function() {
+$( "#draggableTwo" ).draggable({stack: 'div'});
+$('#draggableTwo').addClass('shadow');
+
+});
